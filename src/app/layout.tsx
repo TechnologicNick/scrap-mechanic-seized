2827e93f4fffd716c61025c674dc90f24e6a163b
TechnologicNick <technologicnick@gmail.com>
2025-04-01 21:20:04 +0200

    Add OpenGraph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,23 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "SEIZED - Scrap Mechanic Chapter 2",
   description: "This website has been seized by law enforcement",
+  openGraph: {
+    type: "website",
+    title: "SEIZED - Scrap Mechanic Chapter 2",
+    description: "This website has been seized by law enforcement",
+    url: "https://scrapmechanic.net/chapter-2",
+    siteName: "Scrap Mechanic Chapter 2",
+    locale: "en_US",
+    countryName: "United States",
+    images: [
+      {
+        url: "http://81.172.212.158:25565/scrap-mechanic-seized.jpg",
+        width: 1200,
+        height: 630,
+        alt: "SEIZED - Scrap Mechanic Chapter 2",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
